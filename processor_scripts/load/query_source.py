be2fe7ee1ac10bced170ead7796abcbee5b370fe
panmer <149462342+panmer@users.noreply.github.com>
2024-07-11 05:07:36 +0000

    modfied code query

--- processor_scripts/load/query_source.py
+++ processor_scripts/load/query_source.py
@@ -12,23 +12,26 @@ from org.python.core.util import StringUtil
 errorOccurred=False
 flowFile = session.get()
 
-param = {'place_url':'bernalillo-county','per_page':'100'}
-url = 'https://seeclickfix.com/api/v2/issues?'
+BASE_URL = 'https://seeclickfix.com/api/v2/issues?'
 
 class ModJSON(StreamCallback):
   def __init__(self):
         pass
   def process(self, inputStream, outputStream):
     try:
-        rawreply = urllib2.urlopen(url).read()
-        reply = json.loads(rawreply)
+        param = {'place_url':'clark-county','per_page':'100'} # use
 
-        outputStream.write(bytearray(json.dumps(reply, indent=4).encode('utf-8')))
+        url = BASE_URL + urllib.urlencode(param)
+
+        raw = urllib2.urlopen(url).read()
+        response = json.loads(raw)
+
+        outputStream.write(bytearray(json.dumps(response, indent=4).encode('utf-8')))
     except:
         global errorOccurred
         errorOccurred=True
         
-        outputStream.write(bytearray(json.dumps(reply, indent=4).encode('utf-8')))
+        outputStream.write(bytearray(json.dumps(response, indent=4).encode('utf-8')))
 
 
 if (flowFile != None):
